481826511d9549fd6a93c97c1c806cf8a48c4716
Tuukka Koistinen <tuukka.koistinen@ericsson.com>
2016-09-09 14:57:32 +0300

    add ascii.sh

---
+++ ascii.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+set -u
+
+command -v jp2a >/dev/null 2>&1 || { echo >&2 "Requires jp2a."; exit 1; }
+
+IMAGE=$1
+CODE=$2
+HEIGHT=$3
+
+# some character that does not occur (much) in CODE
+FILLER="'"
+
+TMPFILE=$(mktemp)
+
+function cleanup()
+{
+    rm "${TMPFILE}"
+}
+
+trap cleanup EXIT
+
+jp2a --chars="${FILLER} " --height="${HEIGHT}" "${IMAGE}" > "${TMPFILE}"
+while IFS= read -n1 c ; do
+    # escape the sed substitution command delimiter
+    if [ "${c}" = '/' ] ; then
+        c="\\${c}"
+    fi
+    sed -ie "0,/${FILLER}/ s/${FILLER}/${c}/" "${TMPFILE}"
+    # squash consecutive spaces into one
+done < <(tr '\n' ' ' < "${CODE}" | sed -e 's/ \+/ /g')
+
+cat "${TMPFILE}"
